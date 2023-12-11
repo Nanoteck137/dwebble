35f14e14fdf350e668c09280a82f038f41619083
Patrik M. Rosenström <patrik.millvik@gmail.com>
2023-12-11 15:07:36 +0100

    Added some table columns

--- backend/migrations/20231208195252_init.sql
+++ backend/migrations/20231208195252_init.sql
@@ -1,23 +1,40 @@
 CREATE TABLE IF NOT EXISTS artists (
     id TEXT PRIMARY KEY NOT NULL,
     name TEXT NOT NULL,
-    created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    test TIMESTAMP NOT NULL
+    picture TEXT,
+
+    created_at TIMESTAMP NOT NULL DEFAULT now(),
+    updated_at BIGINT NOT NULL DEFAULT 0
 );
 
 CREATE TABLE IF NOT EXISTS albums (
     id TEXT PRIMARY KEY NOT NULL,
     name TEXT NOT NULL,
     artist_id TEXT NOT NULL,
+    picture TEXT,
+
+    created_at TIMESTAMP NOT NULL DEFAULT now(),
+    updated_at BIGINT NOT NULL DEFAULT 0,
 
     FOREIGN KEY(artist_id) REFERENCES artists(id)
 );
 
 CREATE TABLE IF NOT EXISTS tracks (
     id TEXT PRIMARY KEY NOT NULL,
+    num INT NOT NULL,
     name TEXT NOT NULL,
+    artist_id TEXT NOT NULL,
     album_id TEXT NOT NULL,
 
+    cover_art TEXT,
+    
+    file_quality TEXT NOT NULL,
+    file_mobile TEXT NOT NULL,
+
+    created_at TIMESTAMP NOT NULL DEFAULT now(),
+    updated_at BIGINT NOT NULL DEFAULT 0,
+
+    FOREIGN KEY(artist_id) REFERENCES artists(id),
     FOREIGN KEY(album_id) REFERENCES albums(id)
 );
 
