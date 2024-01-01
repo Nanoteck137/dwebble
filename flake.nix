09f3563d2cce41d8a8129b5319e8fa81bb4d154d
Patrik M. Rosenström <patrik.millvik@gmail.com>
2024-01-02 00:07:46 +0100

    Updated nix dev shell

--- flake.nix
+++ flake.nix
@@ -3,39 +3,24 @@
 
   inputs = {
     nixpkgs.url      = "github:NixOS/nixpkgs/nixos-unstable";
-    rust-overlay.url = "github:oxalica/rust-overlay";
     flake-utils.url  = "github:numtide/flake-utils";
   };
 
-  outputs = { self, nixpkgs, rust-overlay, flake-utils, ... }:
+  outputs = { self, nixpkgs, flake-utils, ... }:
     flake-utils.lib.eachDefaultSystem (system:
       let
-        overlays = [ (import rust-overlay) ];
+        overlays = [];
         pkgs = import nixpkgs {
           inherit system overlays;
-        };
-
-        rustVersion = pkgs.rust-bin.stable.latest.default;
-
-        rustPlatform = pkgs.makeRustPlatform {
-          cargo = rustVersion;
-          rustc = rustVersion;
         };
       in
       {
         devShells.default = pkgs.mkShell {
           buildInputs = with pkgs; [
             nodejs_20
-            openssl
-            pkg-config
             ffmpeg
-            rust-analyzer
+            air
             go
-            
-            (rustVersion.override { extensions = [ "rust-src" ]; }) 
-          ] ++ pkgs.lib.optionals pkgs.stdenv.isDarwin [
-            pkgs.darwin.apple_sdk.frameworks.Security
-            pkgs.darwin.apple_sdk.frameworks.SystemConfiguration
           ];
         };
       }
