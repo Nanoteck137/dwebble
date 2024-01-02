1f5a653846ab8f9d073f17d50bcf159d0459a471
Patrik M. Rosenström <patrik.millvik@gmail.com>
2024-01-02 15:35:36 +0100

    Updated flake with templ

--- flake.nix
+++ flake.nix
@@ -4,9 +4,10 @@
   inputs = {
     nixpkgs.url      = "github:NixOS/nixpkgs/nixos-unstable";
     flake-utils.url  = "github:numtide/flake-utils";
+    templ.url        = "github:a-h/templ";
   };
 
-  outputs = { self, nixpkgs, flake-utils, ... }:
+  outputs = { self, nixpkgs, templ, flake-utils, ... }:
     flake-utils.lib.eachDefaultSystem (system:
       let
         overlays = [];
@@ -20,6 +21,7 @@
             nodejs_20
             ffmpeg
             air
+            templ.packages.${system}.templ
             go
           ];
         };
