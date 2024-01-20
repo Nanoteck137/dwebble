1f7cb420a9dab8cb32c3a76642e8ed776b598906
Patrik M. Rosenström <patrik.millvik@gmail.com>
2024-01-20 18:01:46 +0100

    Added api models

--- types/api.go
+++ types/api.go
@@ -53,3 +53,23 @@ type ApiArtist struct {
 	Name    string `json:"name"`
 	Picture string `json:"picture"`
 }
+
+type ApiAlbum struct {
+	Id       string `json:"id"`
+	Name     string `json:"name"`
+	CoverArt string `json:"coverArt"`
+	ArtistId string `json:"artistId"`
+}
+
+type ApiTrack struct {
+	Id       string `json:"id"`
+	Number     string `json:"number"`
+	Name string `json:"name"`
+	CoverArt string `json:"coverArt"`
+	BestQualityFile string `json:"bestQualityFile"`
+	MobileQualityFile string `json:"mobileQualityFile"`
+	AlbumId string `json:"albumId"`
+	ArtistId string `json:"artistId"`
+	AlbumName string `json:"albumName"`
+	ArtistName string `json:"artistName"`
+}
