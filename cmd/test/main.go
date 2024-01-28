94e7eb1b00dbf9d3b93b99b20c64feee7cb80aa4
Patrik M. Rosenström <patrik.millvik@gmail.com>
2024-01-28 19:08:04 +0100

    Removed collection and sync code

--- cmd/test/main.go
+++ cmd/test/main.go
@@ -8,10 +8,7 @@ import (
 	_ "github.com/doug-martin/goqu/v9/dialect/postgres"
 	"github.com/jackc/pgx/v5/pgxpool"
 	"github.com/joho/godotenv"
-	"github.com/nanoteck137/dwebble/collection"
 	"github.com/nanoteck137/dwebble/database"
-	"github.com/nanoteck137/dwebble/sync"
-	"github.com/nanoteck137/dwebble/types"
 )
 
 func main() {
@@ -32,82 +29,9 @@ func main() {
 		log.Fatal(err)
 	}
 
-	_ = db
-
 	queries := database.New(db)
-	_ = queries
 
 	queries.DeleteAllTracks(ctx)
 	queries.DeleteAllAlbums(ctx)
 	queries.DeleteAllArtists(ctx)
-
-	col, err := collection.ReadFromDir("./mockdata")
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	_ = col
-
-	err = sync.SyncCollection(col, db, types.WorkDir("./work"))
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	return
-
-	// otherartist := collection.ArtistMetadata{
-	// 	Id:     utils.CreateId(),
-	// 	Name:   "Some Other Artist",
-	// 	Albums: []collection.AlbumMetadata{},
-	// }
-
-	// artist := collection.ArtistMetadata{
-	// 	Id:   utils.CreateId(),
-	// 	Name: "Ado",
-	// 	Albums: []collection.AlbumMetadata{
-	// 		{
-	// 			Id:   utils.CreateId(),
-	// 			Name: "unravel",
-	// 			Tracks: []collection.TrackMetadata{
-	// 				{
-	// 					Id:       utils.CreateId(),
-	// 					Name:     "unravel",
-	// 					FilePath: "unravel/01 - unravel.flac",
-	// 				},
-	// 			},
-	// 		},
-	// 		{
-	// 			Id:   utils.CreateId(),
-	// 			Name: "Kura Kura",
-	// 			Tracks: []collection.TrackMetadata{
-	// 				{
-	// 					Id:       utils.CreateId(),
-	// 					Name:     "Kura Kura",
-	// 					FilePath: "Kura Kura/01. クラクラ.flac",
-	// 				},
-	// 			},
-	// 		},
-	// 	},
-	// }
-	//
-	// data, err := json.MarshalIndent(artist, "", "  ")
-	// if err != nil {
-	// 	log.Fatal(err)
-	// }
-	//
-	// fmt.Println(string(data))
-	//
-	// err = os.WriteFile("../testcol/Ado/artist.json", data, 0644)
-	// if err != nil {
-	// 	log.Fatal(err)
-	// }
-
-	// col := collection.New()
-	//
-	// data, err := json.Marshal(col)
-	// if err != nil {
-	// 	log.Fatal(err)
-	// }
-	//
-	// fmt.Println(string(data))
 }
