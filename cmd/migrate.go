b484fac77d6c6132f7894a8f9d548da4f92b792e
Patrik M. Rosenström <patrik.millvik@gmail.com>
2024-01-29 18:18:46 +0100

    Added create and fix commands to migrate

--- cmd/migrate.go
+++ cmd/migrate.go
@@ -33,6 +33,7 @@ var upCmd = &cobra.Command{
 		goose.Up(db, ".")
 	},
 }
+
 var downCmd = &cobra.Command{
 	Use: "down",
 	Run: func(cmd *cobra.Command, args []string) {
@@ -52,10 +53,41 @@ var downCmd = &cobra.Command{
 	},
 }
 
+var createCmd = &cobra.Command{
+	Use: "create MIGRATION_NAME",
+	Args: cobra.ExactArgs(1),
+	Run: func(cmd *cobra.Command, args []string) {
+		godotenv.Load()
+
+		dbUrl := os.Getenv("DB_URL")
+		if dbUrl == "" {
+			log.Fatal("DB_URL not set")
+		}
+
+		db, err := goose.OpenDBWithDriver("pgx", dbUrl)
+		if err != nil {
+			log.Fatalf("goose: failed to open DB: %v\n", err)
+		}
+
+		name := args[0]
+		goose.Create(db, "./sql/schema", name, "sql")
+	},
+}
+
+var fixCmd = &cobra.Command{
+	Use: "fix",
+	Run: func(cmd *cobra.Command, args []string) {
+		goose.Fix("./sql/schema")
+	},
+}
+
 func init() {
 	goose.SetBaseFS(schema.Migrations)
 
 	migrateCmd.AddCommand(upCmd)
 	migrateCmd.AddCommand(downCmd)
+	migrateCmd.AddCommand(createCmd)
+	migrateCmd.AddCommand(fixCmd)
+
 	rootCmd.AddCommand(migrateCmd)
 }
