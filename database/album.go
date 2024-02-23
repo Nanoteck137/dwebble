3290335223301197c8ee2997b80571c43af8920c
Patrik M. Rosenström <patrik.millvik@gmail.com>
2024-02-23 07:36:44 +0100

    Added 'GetAlbumByPath' and 'CreateAlbum'

--- database/album.go
+++ database/album.go
@@ -4,6 +4,7 @@ import (
 	"context"
 
 	"github.com/doug-martin/goqu/v9"
+	"github.com/nanoteck137/dwebble/utils"
 )
 
 type Album struct {
@@ -40,6 +41,52 @@ func (db *Database) GetAlbumById(ctx context.Context, id string) (Album, error) 
 	ds := dialect.From("albums").
 		Select("id", "name", "cover_art", "artist_id", "path").
 		Where(goqu.C("id").Eq(id)).
+		Prepared(true)
+
+	row, err := db.QueryRow(ctx, ds)
+	if err != nil {
+		return Album{}, err
+	}
+
+	var item Album
+	err = row.Scan(&item.Id, &item.Name, &item.CoverArt, &item.ArtistId, &item.Path)
+	if err != nil {
+		return Album{}, err
+	}
+
+	return item, nil
+}
+
+func (db *Database) GetAlbumByPath(ctx context.Context, path string) (Album, error) {
+	ds := dialect.From("albums").
+		Select("id", "name", "cover_art", "artist_id", "path").
+		Where(goqu.C("path").Eq(path)).
+		Prepared(true)
+
+	row, err := db.QueryRow(ctx, ds)
+	if err != nil {
+		return Album{}, err
+	}
+
+	var item Album
+	err = row.Scan(&item.Id, &item.Name, &item.CoverArt, &item.ArtistId, &item.Path)
+	if err != nil {
+		return Album{}, err
+	}
+
+	return item, nil
+}
+
+func (db *Database) CreateAlbum(ctx context.Context, name, coverArt, artistId, path string) (Album, error) {
+	ds := dialect.Insert("albums").
+		Rows(goqu.Record{
+			"id": utils.CreateId(),
+			"name": name,
+			"cover_art": coverArt,
+			"artist_id": artistId,
+			"path": path,
+		}).
+		Returning("id", "name", "cover_art", "artist_id", "path").
 		Prepared(true)
 
 	row, err := db.QueryRow(ctx, ds)
