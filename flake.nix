85577c6235d2e8ee370814a70910086e6155a46b
Patrik M. Rosenström <patrik.millvik@gmail.com>
2024-03-12 05:27:38 +0100

    Removed unused stuff from flake.nix

--- flake.nix
+++ flake.nix
@@ -4,40 +4,22 @@
   inputs = {
     nixpkgs.url      = "github:NixOS/nixpkgs/nixos-unstable";
     flake-utils.url  = "github:numtide/flake-utils";
-    templ.url        = "github:a-h/templ";
   };
 
-  outputs = { self, nixpkgs, templ, flake-utils, ... }:
+  outputs = { self, nixpkgs, flake-utils, ... }:
     flake-utils.lib.eachDefaultSystem (system:
       let
         overlays = [];
         pkgs = import nixpkgs {
           inherit system overlays;
         };
-
-        swag = pkgs.buildGoModule rec {
-          name = "swag";
-          vendorHash = "sha256-BxWmEcx5IIT/yI46CJGE0vE1BRm5zwngc0x1dVy/04s=";
-          src = pkgs.fetchFromGitHub {
-            owner = "swaggo";
-            repo = "swag";
-            rev = "76695ca";
-            hash = "sha256-+YjmYf0BYsjXL8rztPTdQtQhfiTwznGXL/73EzuUu6g=";
-          };
-
-          subPackages = [ "cmd/swag" ];
-        };
       in
       {
         devShells.default = pkgs.mkShell {
           buildInputs = with pkgs; [
-            nodejs_20
             ffmpeg
             air
-            templ.packages.${system}.templ
-            sqlc
             go
-            swag
           ];
         };
       }
