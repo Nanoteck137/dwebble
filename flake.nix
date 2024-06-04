8705e10187dc8f4e3058ff408a3549da2a3eed47
Patrik M. Rosenström <patrik.millvik@gmail.com>
2024-06-04 15:30:24 +0200

    Fixed flake and added version

--- flake.nix
+++ flake.nix
@@ -14,8 +14,26 @@
         pkgs = import nixpkgs {
           inherit system overlays;
         };
+
+        version = pkgs.lib.strings.fileContents "${self}/version";
+        fullVersion = ''${version}-${self.dirtyShortRev or self.shortRev or "dirty"}'';
+
+        app = pkgs.buildGoModule {
+          pname = "dwebble";
+          version = fullVersion;
+          src = ./.;
+
+          ldflags = [
+            "-X github.com/nanoteck137/dwebble/config.Version=${version}"
+            "-X github.com/nanoteck137/dwebble/config.Commit=${self.dirtyRev or self.rev or "no-commit"}"
+          ];
+
+          vendorHash = "sha256-PtNQnTm4//Tdc+VlxC/yXX9yVTJYSFOLTf6wIPVgWzM=";
+        };
       in
       {
+        packages.default = app;
+
         devShells.default = pkgs.mkShell {
           buildInputs = with pkgs; [
             ffmpeg
