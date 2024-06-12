1c1894eac97b423cc9c2612d751dd5c20f4a465e
Patrik M. Rosenström <patrik.millvik@gmail.com>
2024-06-12 02:38:10 +0200

    Working on some testing for the lexer

--- filter/lexer/lexer_test.go
+++ filter/lexer/lexer_test.go
@@ -1,7 +1,6 @@
 package lexer_test
 
 import (
-	"fmt"
 	"strings"
 	"testing"
 
@@ -9,17 +8,42 @@ import (
 	"github.com/nanoteck137/dwebble/filter/token"
 )
 
-func TestLexer(t *testing.T) {
-	src := `"test" hello world ()[]`
-
-	lexer := lexer.New(strings.NewReader(src))
-
-	for {
-		tok := lexer.NextToken()
-		if tok.Kind == token.Eof {
-			break
-		}
-
-		fmt.Printf("tok.Kind: %v\n", tok.Kind)
+func assert(t *testing.T, cond bool, format string, args ...any) {
+	if !cond {
+		t.Fatalf(format, args...)
 	}
+}
+
+type wrapper struct {
+	*lexer.Tokenizer
+
+	current token.Token
+}
+
+func (w *wrapper) next() {
+	w.current = w.NextToken()
+}
+
+func (w *wrapper) expect(t *testing.T, tok token.Kind) {
+	assert(t, w.current.Kind == tok, "Expected token %s got %s", tok.String(), w.current.Kind.String())
+	w.next()
+}
+
+func (w *wrapper) expectIdent(t *testing.T, ident string) {
+	i := w.current.Ident
+	w.expect(t, token.Ident)
+
+	assert(t, i == ident, "Expected ident '%s' got '%s'", ident, i)
+}
+
+func TestLexerIdents(t *testing.T) {
+	src := "hello hello_world _bye_world test123 test_123"
+	w := wrapper{Tokenizer: lexer.New(strings.NewReader(src))}
+	w.next()
+
+	w.expectIdent(t, "hello")
+	w.expectIdent(t, "hello_world")
+	w.expectIdent(t, "_bye_world")
+	w.expectIdent(t, "test123")
+	w.expectIdent(t, "test_123")
 }
