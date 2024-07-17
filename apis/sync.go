42197a7f967d16676ad45f1b24fb14ea757b6a6a
Patrik M. Rosenström <patrik.millvik@gmail.com>
2024-07-17 03:30:23 +0200

    Moving sync handlers to apis

--- apis/sync.go
+++ apis/sync.go
@@ -1,4 +1,4 @@
-package handlers
+package apis
 
 import (
 	"net/http"
@@ -6,28 +6,33 @@ import (
 	"time"
 
 	"github.com/labstack/echo/v4"
+	"github.com/nanoteck137/dwebble/core"
+	"github.com/nanoteck137/dwebble/handlers"
 	"github.com/nanoteck137/dwebble/library"
 	"github.com/nanoteck137/dwebble/log"
 	"github.com/nanoteck137/dwebble/types"
 )
 
-var syncing atomic.Bool
+type syncApi struct {
+	app core.App
+	syncing atomic.Bool
+}
 
-func (h *Handlers) HandleGetSync(c echo.Context) error {
+func (api *syncApi) HandleGetSync(c echo.Context) error {
 	return c.JSON(200, types.NewApiSuccessResponse(types.GetSync{
-		IsSyncing: syncing.Load(),
+		IsSyncing: api.syncing.Load(),
 	}))
 }
 
-func (h *Handlers) HandlePostSync(c echo.Context) error {
+func (api *syncApi) HandlePostSync(c echo.Context) error {
 	go func() {
-		syncing.Store(true)
-		defer syncing.Store(false)
+		api.syncing.Store(true)
+		defer api.syncing.Store(false)
 
 		start := time.Now()
 
 		log.Info("Sync: Reading library")
-		lib, err := library.ReadFromDir(h.libraryDir)
+		lib, err := library.ReadFromDir(api.app.Config().LibraryDir)
 		if err != nil {
 			log.Error("Failed to sync", "err", err)
 			return
@@ -37,37 +42,46 @@ func (h *Handlers) HandlePostSync(c echo.Context) error {
 		start = time.Now()
 
 		log.Info("Sync: Started Syncing library")
-		err = lib.Sync(h.workDir, h.db)
+		err = lib.Sync(api.app.Config().WorkDir(), api.app.DB())
 		if err != nil {
 			log.Error("Failed to sync", "err", err)
 			return
 		}
 		log.Info("Sync: Done Syncing library", "time", time.Since(start))
 
-		h.db.Invalidate()
+		api.app.DB().Invalidate()
 	}()
 
 	return c.JSON(200, types.NewApiSuccessResponse(nil))
 }
 
-func (h *Handlers) InstallSyncHandlers(group Group) {
+func InstallSyncHandlers(app core.App, group handlers.Group) {
+	api := syncApi{
+		app:     app,
+		syncing: atomic.Bool{},
+	}
+
+	requireSetup := RequireSetup(app)
+
 	group.Register(
-		Handler{
+		handlers.Handler{
 			Name:        "GetSyncStatus",
 			Path:        "/sync",
 			Method:      http.MethodGet,
 			DataType:    types.GetSync{},
 			BodyType:    nil,
-			HandlerFunc: h.HandleGetSync,
+			HandlerFunc: api.HandleGetSync,
+			Middlewares: []echo.MiddlewareFunc{requireSetup},
 		},
 
-		Handler{
+		handlers.Handler{
 			Name:        "RunSync",
 			Path:        "/sync",
 			Method:      http.MethodPost,
 			DataType:    nil,
 			BodyType:    nil,
-			HandlerFunc: h.HandlePostSync,
+			HandlerFunc: api.HandlePostSync,
+			Middlewares: []echo.MiddlewareFunc{requireSetup},
 		},
 	)
 }
