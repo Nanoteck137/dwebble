22737222fee7377f6e02633fc2147bcc1a7b2fb8
Patrik M. Rosenström <patrik.millvik@gmail.com>
2024-07-20 13:05:42 +0200

    Cleanup

--- cmd/gen/main.go
+++ cmd/gen/main.go
@@ -2,15 +2,11 @@ package main
 
 import (
 	"encoding/json"
-	"fmt"
 	"os"
 	"reflect"
-	"strconv"
 
-	"github.com/kr/pretty"
 	"github.com/nanoteck137/dwebble/core/log"
 	"github.com/nanoteck137/dwebble/tools/routes"
-	"github.com/nanoteck137/pyrin/ast"
 	"github.com/nanoteck137/pyrin/client"
 	"github.com/nanoteck137/pyrin/extract"
 	"github.com/nanoteck137/pyrin/resolve"
@@ -22,114 +18,6 @@ import (
 //  - Parse omit flag
 //  - General code cleanup
 
-type Context struct {
-	types    map[string]reflect.Type
-	nameUsed map[string]int
-	names    map[string]string
-}
-
-func (c *Context) RegisterName(name, pkg string) string {
-	fullName := pkg + "-" + name
-
-	used, exists := c.nameUsed[name]
-	if !exists {
-		c.nameUsed[name] = 1
-		c.names[fullName] = name
-
-		return name
-	} else {
-		c.nameUsed[name] = used + 1
-
-		newName := name + strconv.Itoa(used+1)
-		c.names[fullName] = newName
-
-		return newName
-	}
-}
-
-func (c *Context) translateName(name, pkg string) string {
-	fullName := pkg + "-" + name
-
-	n, exists := c.names[fullName]
-	if !exists {
-		log.Fatal("Name not registered", "name", name, "pkg", pkg)
-	}
-
-	return n
-}
-
-func (c *Context) getType(t reflect.Type) ast.Typespec {
-	switch t.Kind() {
-	case reflect.Int, reflect.Int8, reflect.Int16, reflect.Int32, reflect.Int64:
-		return &ast.IdentTypespec{Ident: "int"}
-	case reflect.Bool:
-		return &ast.IdentTypespec{Ident: "int"}
-	case reflect.String:
-		return &ast.IdentTypespec{Ident: "string"}
-	case reflect.Struct:
-		fullName := t.PkgPath() + "-" + t.Name()
-		n, exists := c.names[fullName]
-		if !exists {
-			name := c.RegisterName(t.Name(), t.PkgPath())
-
-			_, exists := c.types[name]
-			if !exists {
-				c.types[name] = t
-			}
-
-			n = name
-		}
-
-		return &ast.IdentTypespec{Ident: n}
-	case reflect.Slice:
-		el := c.getType(t.Elem())
-		return &ast.ArrayTypespec{
-			Element: el,
-		}
-	default:
-		log.Fatal("Unknown type", "name", t.Name(), "kind", t.Kind())
-	}
-
-	return nil
-}
-
-func printIndent(indent int) {
-	if indent == 0 {
-		return
-	}
-
-	for i := 0; i < indent; i++ {
-		fmt.Print("  ")
-	}
-}
-
-func checkType(t reflect.Type, indent int) {
-	switch t.Kind() {
-	case reflect.Struct:
-		printStruct(t, indent+1)
-	case reflect.Slice:
-		checkType(t.Elem(), indent)
-	}
-}
-
-func printStruct(t reflect.Type, indent int) {
-	if t.Kind() != reflect.Struct {
-		log.Fatal("Type needs to be struct")
-	}
-
-	printIndent(indent)
-	fmt.Println("Name: ", t.Name())
-
-	for i := 0; i < t.NumField(); i++ {
-		sf := t.Field(i)
-
-		// printIndent(indent)
-		// fmt.Printf("sf.Type.Kind(): %v\n", sf.Type.Kind())
-
-		checkType(sf.Type, indent)
-	}
-}
-
 func main() {
 	routes := routes.ServerRoutes(nil)
 
@@ -137,26 +25,22 @@ func main() {
 
 	resolver := resolve.New()
 
-	c2 := extract.NewContext()
+	c := extract.NewContext()
 
 	for _, route := range routes {
 		if route.Data != nil {
-			c2.ExtractTypes(route.Data)
+			c.ExtractTypes(route.Data)
 		}
 
 		if route.Body != nil {
-			c2.ExtractTypes(route.Body)
+			c.ExtractTypes(route.Body)
 		}
 	}
 
-	pretty.Println(c2)
-
-	decls, err := c2.ConvertToDecls()
+	decls, err := c.ConvertToDecls()
 	if err != nil {
 		log.Fatal("Failed to convert extract context to decls", "err", err)
 	}
-
-	pretty.Println(decls)
 
 	for _, decl := range decls {
 		resolver.AddSymbolDecl(decl)
@@ -169,7 +53,7 @@ func main() {
 		if route.Data != nil {
 			t := reflect.TypeOf(route.Data)
 
-			name, err := c2.TranslateName(t.Name(), t.PkgPath())
+			name, err := c.TranslateName(t.Name(), t.PkgPath())
 			if err != nil {
 				log.Fatal("Failed to translate name", "name", t.Name(), "pkg", t.PkgPath(), "err", err)
 			}
@@ -185,7 +69,7 @@ func main() {
 		if route.Body != nil {
 			t := reflect.TypeOf(route.Body)
 
-			name, err := c2.TranslateName(t.Name(), t.PkgPath())
+			name, err := c.TranslateName(t.Name(), t.PkgPath())
 			if err != nil {
 				log.Fatal("Failed to translate name", "name", t.Name(), "pkg", t.PkgPath(), "err", err)
 			}
@@ -249,5 +133,5 @@ func main() {
 		log.Fatal("Failed to write pyrin.json", "err", err)
 	}
 
-	log.Info("Wrote 'pyrin.json'")
+	log.Info("Wrote 'misc/pyrin.json'")
 }
