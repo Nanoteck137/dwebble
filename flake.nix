3a42a8007c272b4135a8b7d3cf3f8b7358f29d91
Patrik M. Rosenström <patrik.millvik@gmail.com>
2024-07-31 04:18:56 +0200

    Updated vendorHash

--- flake.nix
+++ flake.nix
@@ -27,7 +27,7 @@
             "-X github.com/nanoteck137/dwebble/config.Commit=${self.dirtyRev or self.rev or "no-commit"}"
           ];
 
-          vendorHash = "sha256-drGI4xNN9EHzc6Fl3jPjXXScrMXk9XnHnciKAe7WXQs=";
+          vendorHash = "sha256-KA2J8ag8CZCipMBLIZX+uGB0U6sIlX1h7eavFoAVBlk=";
         };
       in
       {
