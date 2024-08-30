c0536dfc836ea4dc8258194a2df6b3deaf9ee227
Patrik M. Rosenström <patrik.millvik@gmail.com>
2024-08-30 18:36:01 +0200

    Testing

--- web/src/routes/+page.svelte
+++ web/src/routes/+page.svelte
@@ -1,3 +1,20 @@
-<script lang="ts"></script>
+<script lang="ts">
+  import { flip } from "svelte/animate";
+  import { fade, fly, slide } from "svelte/transition";
+
+  let popups = $state<string[]>([]);
+</script>
 
 <p class="p-4 text-xl">Home Page</p>
+
+<button
+  onclick={() => {
+    popups.push("Hello " + Date.now());
+  }}>Test</button
+>
+
+{#each popups as popup (popup)}
+  <div animate:flip={{ duration: 2000 }}>
+    <p>{popup}</p>
+  </div>
+{/each}
