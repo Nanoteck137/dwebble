1077edecea25a1f9743f89ce165b959b712d43ba
Patrik M. Rosenström <patrik.millvik@gmail.com>
2024-09-04 03:01:12 +0200

    Work on the track editor

--- web/src/routes/server/edit/album/[id]/+page.svelte
+++ web/src/routes/server/edit/album/[id]/+page.svelte
@@ -1,4 +1,6 @@
 <script lang="ts">
+  import { applyAction, enhance } from "$app/forms";
+  import { invalidate, invalidateAll } from "$app/navigation";
   import { Track } from "$lib/api/types.js";
   import { musicManager } from "$lib/music-manager.js";
   import { trackToMusicTrack } from "$lib/utils.js";
@@ -14,6 +16,11 @@
 
   let deleteTrack = $state<Track>();
   let confirmTrackDeletionDialog = $state<HTMLDialogElement>();
+
+  function openDeleteConfirm(track: Track) {
+    deleteTrack = track;
+    confirmTrackDeletionDialog?.showModal();
+  }
 
   let editAlbumArtist = $state<HTMLDialogElement>();
 
@@ -75,40 +82,47 @@
 
 <p>{data.album.coverArt}</p>
 
-<div class="flex flex-col">
-  {#each data.tracks as track (track.id)}
-    <div class="flex gap-2">
-      <button
+<!-- <button
         class="text-blue-400"
         onclick={() => {
           musicManager.clearQueue();
           musicManager.addTrackToQueue(trackToMusicTrack(track), true);
         }}>Play</button
-      >
+      > -->
 
-      <div class="flex flex-col">
-        <p>
+<!-- <p>
           {#if track.number}
             <span>{track.number} - </span>
           {/if}
           <span>{track.name}</span>
         </p>
-        <p>Tags: {track.tags.join(", ")}</p>
-      </div>
+        <p>Tags: {track.tags.join(", ")}</p> -->
 
-      <button
-        class="text-red-400"
-        onclick={() => {
-          deleteTrack = track;
-          confirmTrackDeletionDialog?.showModal();
-        }}>Delete</button
-      >
-      <button
-        class="text-blue-400"
-        onclick={() => {
-          openTrackEditor(track);
-        }}>Edit</button
-      >
+<!-- -->
+
+<div class="flex flex-col">
+  {#each data.tracks as track (track.id)}
+    <div class="border-b">
+      <p>{track.name}</p>
+      <div class="px-4">
+        <p>Number: {track.number == null ? "NULL" : track.number}</p>
+        <p>Year: {track.year == null ? "NULL" : track.year}</p>
+        <p>Tags: {track.tags.join(", ")}</p>
+
+        <button
+          class="rounded bg-blue-400 px-2 py-1"
+          onclick={() => {
+            openTrackEditor(track);
+          }}>Edit</button
+        >
+
+        <button
+          class="rounded bg-red-400 px-2 py-1"
+          onclick={() => {
+            openDeleteConfirm(track);
+          }}>Delete</button
+        >
+      </div>
     </div>
   {/each}
 </div>
@@ -183,6 +197,24 @@
 
 <dialog
   class="rounded bg-[--bg-color] p-4 text-[--fg-color] backdrop:bg-black/45"
+  bind:this={confirmTrackDeletionDialog}
+>
+  <p>Are you sure?</p>
+
+  <button
+    onclick={() => {
+      confirmTrackDeletionDialog?.close();
+    }}>Close</button
+  >
+
+  <form action="?/deleteTrack" method="post">
+    <input name="trackId" value={deleteTrack?.id} type="hidden" />
+    <button>DELETE</button>
+  </form>
+</dialog>
+
+<dialog
+  class="rounded bg-[--bg-color] p-4 text-[--fg-color] backdrop:bg-black/45"
   bind:this={editAlbumArtist}
 >
   <form action="?/editAlbumArtist" method="post">
@@ -204,7 +236,21 @@
   class="w-[400px] rounded bg-[--bg-color] p-4 text-[--fg-color] backdrop:bg-black/45"
   bind:this={editTrackDialog}
 >
-  <form class="flex flex-col" action="?/editTrack" method="post">
+  <form
+    class="flex flex-col"
+    action="?/editTrack"
+    method="post"
+    use:enhance={() => {
+      return async ({ result }) => {
+        await applyAction(result);
+
+        if (result.type === "success") {
+          editTrackDialog?.close();
+          await invalidateAll();
+        }
+      };
+    }}
+  >
     <input
       class="text-black"
       name="trackId"
