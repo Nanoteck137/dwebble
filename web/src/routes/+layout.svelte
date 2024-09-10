5960d18028ed90b442337db61d341952e0aed0b5
Patrik M. Rosenström <patrik.millvik@gmail.com>
2024-09-10 04:24:07 +0200

    Using transition api

--- web/src/routes/+layout.svelte
+++ web/src/routes/+layout.svelte
@@ -16,7 +16,7 @@
   import AudioPlayer from "$lib/components/AudioPlayer.svelte";
   import Link from "$lib/components/Link.svelte";
   import { browser } from "$app/environment";
-  import { fade } from "svelte/transition";
+  import { fade, fly } from "svelte/transition";
 
   let { children, data } = $props();
 
@@ -65,67 +65,68 @@
     }}
     transition:fade
   ></button>
-{/if}
 
-<aside
-  class={`fixed bottom-0 top-0 z-50 flex w-80 flex-col bg-[--bg-color] px-4 text-[--fg-color] transition-transform duration-300 ${showSideMenu ? "translate-x-0" : "-translate-x-[100%]"}`}
->
-  <div class="flex h-16 items-center gap-4 px-2 py-2">
-    <button
-      onclick={() => {
-        showSideMenu = false;
-      }}
-    >
-      <Menu size="32" />
-    </button>
-    <a
-      class="text-3xl font-medium text-[--logo-color]"
-      href="/"
-      onclick={() => {
-        showSideMenu = false;
-      }}
-    >
-      Dwebble
-    </a>
-  </div>
+  <aside
+    class={`text-[--fg-color]"} fixed bottom-0 top-0 z-50 flex w-80 flex-col bg-[--bg-color] px-4`}
+    transition:fly={{ x: -400 }}
+  >
+    <div class="flex h-16 items-center gap-4 px-2 py-2">
+      <button
+        onclick={() => {
+          showSideMenu = false;
+        }}
+      >
+        <Menu size="32" />
+      </button>
+      <a
+        class="text-3xl font-medium text-[--logo-color]"
+        href="/"
+        onclick={() => {
+          showSideMenu = false;
+        }}
+      >
+        Dwebble
+      </a>
+    </div>
 
-  <div class="flex flex-col gap-2 py-2">
-    <Link title="Home" href="/" icon={Home} onClick={close} />
-    <Link title="Artists" href="/artists" icon={Users} onClick={close} />
-    <Link title="Albums" href="/albums" icon={DiscAlbum} onClick={close} />
-    <Link title="Tracks" href="/tracks" icon={FileMusic} onClick={close} />
+    <div class="flex flex-col gap-2 py-2">
+      <Link title="Home" href="/" icon={Home} onClick={close} />
+      <Link title="Artists" href="/artists" icon={Users} onClick={close} />
+      <Link title="Albums" href="/albums" icon={DiscAlbum} onClick={close} />
+      <Link title="Tracks" href="/tracks" icon={FileMusic} onClick={close} />
 
-    {#if data.user}
-      <Link
-        title="Playlists"
-        href="/playlists"
-        icon={ListMusic}
-        onClick={close}
-      />
-    {/if}
-  </div>
-  <div class="flex-grow"></div>
-  <div class="flex flex-col gap-2 py-4">
-    {#if data.user}
-      <Link
-        title={data.user.username}
-        href="/account"
-        icon={User}
-        onClick={close}
-      />
-
-      {#if data.user.isOwner}
-        <Link title="Server" href="/server" icon={Server} onClick={close} />
+      {#if data.user}
+        <Link
+          title="Playlists"
+          href="/playlists"
+          icon={ListMusic}
+          onClick={close}
+        />
       {/if}
+    </div>
+    <div class="flex-grow"></div>
+    <div class="flex flex-col gap-2 py-4">
+      {#if data.user}
+        <Link
+          title={data.user.username}
+          href="/account"
+          icon={User}
+          onClick={close}
+        />
 
-      <form class="w-full" action="/logout" method="POST">
-        <Link title="Logout" icon={LogOut} onClick={close} />
-      </form>
-    {:else}
-      <Link title="Login" href="/login" icon={LogIn} onClick={close} />
-    {/if}
-  </div>
-</aside>
+        {#if data.user.isOwner}
+          <Link title="Server" href="/server" icon={Server} onClick={close} />
+        {/if}
+
+        <form class="w-full" action="/logout" method="POST">
+          <Link title="Logout" icon={LogOut} onClick={close} />
+        </form>
+      {:else}
+        <Link title="Login" href="/login" icon={LogIn} onClick={close} />
+      {/if}
+    </div>
+  </aside>
+{/if}
 
 <svelte:head>
   <title>Dwebble</title>
