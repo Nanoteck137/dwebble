9b323458d5e7edf0889ea87c2930f97ca3546c88
Patrik M. Rosenström <patrik.millvik@gmail.com>
2024-11-24 22:58:28 +0100

    Testing new client impl

--- cmd/dwebble-cli/api/base.go
+++ cmd/dwebble-cli/api/base.go
@@ -27,7 +27,7 @@ func (c *Client) SetToken(token string) {
 
 type Options struct {
 	QueryParams map[string]string
-	Boundary string
+	Boundary    string
 }
 
 func createUrl(addr, path string, query map[string]string) (string, error) {
@@ -47,10 +47,13 @@ func createUrl(addr, path string, query map[string]string) (string, error) {
 	return u.String(), nil
 }
 
+type ErrorType string
+
 type ApiError[E any] struct {
-	Code    int    `json:"code"`
-	Message string `json:"message"`
-	Errors  E      `json:"errors,omitempty"`
+	Code    int       `json:"code"`
+	Type    ErrorType `json:"type"`
+	Message string    `json:"message"`
+	Extra   E         `json:"extra,omitempty"`
 }
 
 func (err *ApiError[E]) Error() string {
@@ -58,9 +61,9 @@ func (err *ApiError[E]) Error() string {
 }
 
 type ApiResponse[D any, E any] struct {
-	Status string       `json:"status"`
-	Data   D            `json:"data,omitempty"`
-	Error  *ApiError[E] `json:"error,omitempty"`
+	Success bool         `json:"success"`
+	Data    D            `json:"data,omitempty"`
+	Error   *ApiError[E] `json:"error,omitempty"`
 }
 
 type RequestData struct {
@@ -117,7 +120,7 @@ func Request[D any](data RequestData) (*D, error) {
 		return nil, err
 	}
 
-	if res.Status == "error" {
+	if !res.Success {
 		return nil, res.Error
 	}
 
@@ -146,7 +149,7 @@ func RequestForm[D any](data RequestData, boundary string, body Reader) (*D, err
 		return nil, err
 	}
 
-	if res.Status == "error" {
+	if !res.Success {
 		return nil, res.Error
 	}
 
