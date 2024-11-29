956b770f6f181bbe6aa9359d5b17f5647797f7e7
Patrik M. Rosenström <patrik.millvik@gmail.com>
2024-11-29 04:01:54 +0100

    Better layout for album page

--- web/src/routes/albums/[id]/+page.svelte
+++ web/src/routes/albums/[id]/+page.svelte
@@ -13,49 +13,46 @@
   let { data } = $props();
 </script>
 
-{#snippet header()}
-  <div class="w-64">
-    <img
-      class="aspect-square w-64 rounded object-cover"
-      src={data.album.coverArt.large}
-      alt=""
-    />
-    <div class="h-2"></div>
+<div class="py-2">
+  <Breadcrumb.Root>
+    <Breadcrumb.List>
+      <Breadcrumb.Item>
+        <Breadcrumb.Link href="/albums">Albums</Breadcrumb.Link>
+      </Breadcrumb.Item>
+      <Breadcrumb.Separator />
+      <Breadcrumb.Item>
+        <Breadcrumb.Page>{data.album.name}</Breadcrumb.Page>
+      </Breadcrumb.Item>
+    </Breadcrumb.List>
+  </Breadcrumb.Root>
+</div>
 
-    <p class="line-clamp-2 text-center font-bold" title={data.album.name}>
-      {data.album.name}
-    </p>
-    <p class="line-clamp-1 text-center text-xs" title={data.album.artistName}>
-      {data.album.artistName}
-    </p>
+<div class="flex gap-2">
+  <img
+    class="inline-flex aspect-square w-48 min-w-48 items-center justify-center rounded border object-cover text-xs"
+    src=""
+    alt="cover"
+  />
 
-    <div class="h-2"></div>
-    <div class="flex items-center justify-center gap-5">
-      <Button class="rounded-full" size="icon" variant="outline">
-        <ListPlus />
-      </Button>
+  <div class="flex flex-col py-2">
+    <div class="flex flex-col">
+      <p class="font-bold">{data.album.name}</p>
+      <a class="text-xs hover:underline" href="/artists/{data.album.artistId}">
+        {data.album.artistName}
+      </a>
+    </div>
 
-      <Button
-        class="rounded-full p-2"
-        size="icon-lg"
-        variant="default"
-        onclick={() => {
-          musicManager.clearQueue();
+    <div class="flex-grow"></div>
 
-          data.tracks.forEach((t) =>
-            musicManager.addTrackToQueue(trackToMusicTrack(t)),
-          );
-        }}
-      >
+    <div>
+      <Button variant="outline">
         <Play />
+        Play
       </Button>
 
       <DropdownMenu.Root>
         <DropdownMenu.Trigger
-          class={cn(
-            buttonVariants({ variant: "outline", size: "icon" }),
-            "rounded-full",
-          )}
+          class={buttonVariants({ variant: "outline", size: "icon" })}
         >
           <EllipsisVertical />
         </DropdownMenu.Trigger>
@@ -74,116 +71,93 @@
         </DropdownMenu.Content>
       </DropdownMenu.Root>
     </div>
-    <div class="h-2"></div>
   </div>
-{/snippet}
-
-<div class="py-2">
-  <Breadcrumb.Root>
-    <Breadcrumb.List>
-      <Breadcrumb.Item>
-        <Breadcrumb.Link href="/albums">Albums</Breadcrumb.Link>
-      </Breadcrumb.Item>
-      <Breadcrumb.Separator />
-      <Breadcrumb.Item>
-        <Breadcrumb.Page>{data.album.name}</Breadcrumb.Page>
-      </Breadcrumb.Item>
-    </Breadcrumb.List>
-  </Breadcrumb.Root>
 </div>
 
+<div class="h-4"></div>
+
 <div class="flex flex-col gap-2">
-  <div class="flex justify-center md:fixed md:h-full">
-    {@render header()}
-  </div>
-  <div class="md:ml-5 md:pl-64">
-    <div class="flex flex-col gap-2">
-      {#each data.tracks as track, i}
-        <div class="group flex items-center gap-2 py-1 pr-4">
-          <p
-            class="min-w-10 text-right text-sm font-medium group-hover:hidden"
-          >
-            {#if track.number}
-              <span>{track.number}.</span>
-            {/if}
-          </p>
-          <button
-            class="hidden h-10 w-10 items-center justify-end group-hover:flex"
-            onclick={() => {
-              musicManager.clearQueue();
+  {#each data.tracks as track, i}
+    <div class="group flex items-center gap-2 py-1 pr-4">
+      <p class="min-w-10 text-right text-sm font-medium group-hover:hidden">
+        {#if track.number}
+          <span>{track.number}.</span>
+        {/if}
+      </p>
+      <button
+        class="hidden h-10 w-10 items-center justify-end group-hover:flex"
+        onclick={() => {
+          musicManager.clearQueue();
 
-              data.tracks.forEach((t) =>
-                musicManager.addTrackToQueue(trackToMusicTrack(t), false),
-              );
+          data.tracks.forEach((t) =>
+            musicManager.addTrackToQueue(trackToMusicTrack(t), false),
+          );
 
-              const index = data.tracks.findIndex((t) => t.id == track.id);
-              musicManager.setQueueIndex(index);
+          musicManager.setQueueIndex(i);
 
-              musicManager.requestPlay();
-            }}
-          >
-            <Play size="20" />
-          </button>
-          <div class="flex flex-grow flex-col py-1">
-            <p class="line-clamp-1 text-sm font-medium" title={track.name}>
-              {track.name}
-            </p>
-            <a
-              class="line-clamp-1 w-fit text-xs hover:underline"
-              title={track.artistName}
-              href={`/artists/${track.artistId}`}
-            >
-              {track.artistName}
-            </a>
-          </div>
-          <div class="flex items-center gap-2">
-            <p class="text-xs">
-              {formatTime(track.duration ?? 0)}
-            </p>
-            <!-- <button
+          musicManager.requestPlay();
+        }}
+      >
+        <Play size="20" />
+      </button>
+      <div class="flex flex-grow flex-col py-1">
+        <p class="line-clamp-1 text-sm font-medium" title={track.name}>
+          {track.name}
+        </p>
+        <a
+          class="line-clamp-1 w-fit text-xs hover:underline"
+          title={track.artistName}
+          href={`/artists/${track.artistId}`}
+        >
+          {track.artistName}
+        </a>
+      </div>
+      <div class="flex items-center gap-2">
+        <p class="text-xs">
+          {formatTime(track.duration ?? 0)}
+        </p>
+        <!-- <button
               class="hidden rounded-full p-1 hover:bg-black/20 group-hover:block"
             >
             </button> -->
 
-            <DropdownMenu.Root>
-              <DropdownMenu.Trigger
-                class={cn(
-                  buttonVariants({ variant: "ghost", size: "icon" }),
-                  "rounded-full",
-                )}
-              >
-                <EllipsisVertical />
-              </DropdownMenu.Trigger>
-              <DropdownMenu.Content align="end">
-                <DropdownMenu.Group>
-                  <DropdownMenu.Item>
-                    <button
-                      class="flex w-full items-center gap-2"
-                      onclick={() => {
-                        musicManager.clearQueue();
+        <DropdownMenu.Root>
+          <DropdownMenu.Trigger
+            class={cn(
+              buttonVariants({ variant: "ghost", size: "icon" }),
+              "rounded-full",
+            )}
+          >
+            <EllipsisVertical />
+          </DropdownMenu.Trigger>
+          <DropdownMenu.Content align="end">
+            <DropdownMenu.Group>
+              <DropdownMenu.Item>
+                <button
+                  class="flex w-full items-center gap-2"
+                  onclick={() => {
+                    musicManager.clearQueue();
 
-                        data.tracks.forEach((t) =>
-                          musicManager.addTrackToQueue(
-                            trackToMusicTrack(t),
-                            false,
-                          ),
-                        );
+                    data.tracks.forEach((t) =>
+                      musicManager.addTrackToQueue(
+                        trackToMusicTrack(t),
+                        false,
+                      ),
+                    );
 
-                        musicManager.setQueueIndex(i);
-                        musicManager.requestPlay();
-                      }}
-                    >
-                      <Play size="16" />
-                      Play
-                    </button>
-                  </DropdownMenu.Item>
-                </DropdownMenu.Group>
-              </DropdownMenu.Content>
-            </DropdownMenu.Root>
-          </div>
-        </div>
-        <Separator />
-      {/each}
+                    musicManager.setQueueIndex(i);
+                    musicManager.requestPlay();
+                  }}
+                >
+                  <Play size="16" />
+                  Play
+                </button>
+              </DropdownMenu.Item>
+            </DropdownMenu.Group>
+          </DropdownMenu.Content>
+        </DropdownMenu.Root>
+      </div>
     </div>
-  </div>
+    <Separator />
+  {/each}
 </div>
