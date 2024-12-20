5ef8568d3d6d822a6fb6f8783df3bf46073f8577
Patrik M. Rosenström <patrik.millvik@gmail.com>
2024-12-20 18:55:13 +0100

    Working on new track importing page

--- web/src/routes/albums/[id]/edit/import/+page.svelte
+++ web/src/routes/albums/[id]/edit/import/+page.svelte
@@ -1,31 +1,81 @@
 <script lang="ts">
   import { goto } from "$app/navigation";
+  import { openArtistQuery, type Artist } from "$lib";
   import { ApiClient } from "$lib/api/client";
+  import type { UploadTrackBody } from "$lib/api/types.js";
   import {
     Breadcrumb,
     Button,
     Card,
     Input,
     Label,
+    Separator,
   } from "@nanoteck137/nano-ui";
+  import { Plus, X } from "lucide-svelte";
 
   const { data } = $props();
 
-  let uploading = $state(false);
+  const apiClient = new ApiClient(data.apiAddress);
+  apiClient.setToken(data.userToken);
 
-  async function submit(formData: FormData) {
-    // const apiClient = new ApiClient(data.apiAddress);
-    // apiClient.setToken(data.userToken);
-    // const res = await apiClient.uploadTrack(data.album.id, formData);
-    // uploading = false;
-    // if (!res.success) {
-    //   // TODO(patrik): Toast
-    //   throw res.error.message;
-    // }
-    // // TODO(patrik): Not the best solution, but i need the browser to
-    // // refresh for the new image to show
-    // // window.location.href = `/albums/${data.album.id}/edit`;
-    // goto(`/albums/${data.album.id}/edit`, { invalidateAll: true });
+  const albumArtist = $state<Artist>({
+    name: data.album.artistName.default,
+    id: data.album.artistId,
+  });
+
+  type Track = {
+    name: string;
+    otherName: string;
+    artist: Artist;
+    num?: number;
+    year?: number;
+    tags: string;
+    extraArtists: Artist[];
+    file: File;
+  };
+
+  let uploadState = $state({
+    uploading: false,
+    currentTrack: 0,
+    numTracks: 0,
+  });
+
+  let fileSelector = $state<HTMLInputElement>();
+  let tracks = $state<Track[]>([]);
+
+  async function submit() {
+    const apiClient = new ApiClient(data.apiAddress);
+    apiClient.setToken(data.userToken);
+
+    uploadState.uploading = true;
+    uploadState.numTracks = tracks.length;
+    uploadState.currentTrack = 1;
+    for (const track of tracks) {
+      const body: UploadTrackBody = {
+        name: track.name,
+        otherName: track.otherName,
+        albumId: data.album.id,
+        artistId: track.artist.id,
+        number: track.num ?? 0,
+        year: track.year ?? 0,
+        extraArtists: track.extraArtists.map((a) => a.id),
+        tags: track.tags.split(","),
+      };
+
+      const trackData = new FormData();
+      trackData.set("body", JSON.stringify(body));
+      trackData.set("track", track.file);
+
+      const res = await apiClient.uploadTrack(trackData);
+      if (!res.success) {
+        // TODO(patrik): Toast
+        console.log(res.error.message);
+      }
+      uploadState.currentTrack += 1;
+    }
+    uploadState.uploading = false;
+
+    goto(`/albums/${data.album.id}/edit`, { invalidateAll: true });
   }
 </script>
 
@@ -58,9 +108,7 @@
 <form
   onsubmit={(e) => {
     e.preventDefault();
-    uploading = true;
-    const formData = new FormData(e.target as HTMLFormElement);
-    submit(formData);
+    submit();
   }}
 >
   <Card.Root class="mx-auto w-full max-w-[560px]">
@@ -69,46 +117,172 @@
       <Card.Description>{data.album.name.default}</Card.Description>
     </Card.Header>
     <Card.Content class="flex flex-col gap-4">
-      <div class="flex flex-col gap-2">
-        <Label for="tracks">Tracks</Label>
-        <Input
-          id="tracks"
-          name="files"
-          type="file"
-          multiple
-          accept="audio/*"
-        />
-      </div>
+      {#each tracks as track}
+        <p>{track.file.name}</p>
+        <div class="flex flex-col gap-2 sm:flex-row">
+          <div class="flex flex-col gap-2 sm:max-w-24">
+            <Label for="trackNumber">Number</Label>
+            <Input
+              id="trackNumber"
+              type="number"
+              bind:value={track.num}
+              tabindex={1}
+            />
+          </div>
+
+          <div class="w-full">
+            <Label for="trackName">Name</Label>
+            <Input
+              id="trackName"
+              type="text"
+              tabindex={2}
+              bind:value={track.name}
+            />
+          </div>
+        </div>
+
+        <div class="flex flex-col gap-2 sm:flex-row">
+          <div class="flex flex-col gap-2 sm:max-w-24">
+            <Label for="trackYear">Year</Label>
+            <Input
+              id="trackYear"
+              type="number"
+              bind:value={track.year}
+              tabindex={3}
+            />
+          </div>
+
+          <div class="flex w-full flex-col gap-2">
+            <Label for="trackOtherName">Other Name</Label>
+            <Input
+              id="trackOtherName"
+              type="text"
+              tabindex={4}
+              bind:value={track.otherName}
+            />
+          </div>
+        </div>
+
+        <div class="flex flex-col gap-2">
+          <Label for="trackTags">Tags</Label>
+          <Input
+            id="trackTags"
+            type="text"
+            tabindex={5}
+            bind:value={track.tags}
+          />
+        </div>
+
+        <div class="flex flex-1 flex-col gap-2">
+          <Label>Artist</Label>
+          <Button
+            class="justify-start"
+            variant="ghost"
+            onclick={async () => {
+              const res = await openArtistQuery({ apiClient });
+              if (res) {
+                track.artist = res;
+              }
+            }}
+            tabindex={6}
+          >
+            {track.artist.name}
+          </Button>
+        </div>
+
+        <Label class="flex items-center gap-2">
+          Extra Artists
+          <button
+            type="button"
+            class="hover:cursor-pointer"
+            tabindex={7}
+            onclick={async () => {
+              const res = await openArtistQuery({ apiClient });
+              if (res) {
+                const index = track.extraArtists.findIndex(
+                  (a) => a.id === res.id,
+                );
+                if (index === -1) {
+                  track.extraArtists.push(res);
+                }
+              }
+            }}
+          >
+            <Plus size="16" />
+          </button>
+        </Label>
+
+        <div class="flex flex-wrap gap-2">
+          {#each track.extraArtists as artist, i}
+            <p
+              class="flex w-fit items-center gap-1 rounded-full bg-white px-2 py-1 text-xs text-black"
+              title={`${artist.id}: ${artist.name}`}
+            >
+              <button
+                type="button"
+                class="text-red-400 hover:cursor-pointer"
+                onclick={() => {
+                  track.extraArtists.splice(i, 1);
+                }}
+              >
+                <X size="16" />
+              </button>
+              {artist.name}
+            </p>
+          {/each}
+        </div>
+
+        <Separator />
+      {/each}
+
+      <Button
+        variant="outline"
+        onclick={() => {
+          fileSelector?.click();
+        }}
+      >
+        Open File Selector
+      </Button>
+
+      {#if uploadState.uploading}
+        <p>Uploading: {uploadState.currentTrack} / {uploadState.numTracks}</p>
+      {/if}
     </Card.Content>
     <Card.Footer class="flex justify-end gap-4">
       <Button href="/albums/{data.album.id}/edit" variant="outline">
         Back
       </Button>
-      <Button type="submit" disabled={uploading}>
-        Save
-        {#if uploading}
-          <!-- TODO(patrik): Move to component and fix styling-->
-          <div role="status">
-            <svg
-              aria-hidden="true"
-              class="h-4 w-4 animate-spin fill-blue-600 text-gray-200 dark:text-gray-600"
-              viewBox="0 0 100 101"
-              fill="none"
-              xmlns="http://www.w3.org/2000/svg"
-            >
-              <path
-                d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z"
-                fill="currentColor"
-              />
-              <path
-                d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z"
-                fill="currentFill"
-              />
-            </svg>
-            <span class="sr-only">Loading...</span>
-          </div>
-        {/if}
-      </Button>
+      <Button
+        type="submit"
+        disabled={tracks.length <= 0 || uploadState.uploading}>Save</Button
+      >
     </Card.Footer>
   </Card.Root>
 </form>
+
+<input
+  class="hidden"
+  bind:this={fileSelector}
+  type="file"
+  multiple
+  accept="audio/*"
+  onchange={(e) => {
+    const input = e.target as HTMLInputElement;
+    if (!input.files) return;
+
+    const files = input.files;
+    for (let i = 0; i < files.length; i++) {
+      const file = files.item(i);
+      if (file) {
+        tracks.push({
+          name: file.name,
+          otherName: "",
+          extraArtists: [],
+          file,
+          artist: albumArtist,
+          tags: "",
+        });
+      }
+    }
+  }}
+/>
