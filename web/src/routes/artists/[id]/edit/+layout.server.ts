70268a55ffc4d5d34a021c85007eda6192816d7a
Patrik M. Rosenström <patrik.millvik@gmail.com>
2025-02-08 15:20:05 +0100

    Added page guard to artist editing page, fixes #125

--- web/src/routes/artists/[id]/edit/+layout.server.ts
+++ web/src/routes/artists/[id]/edit/+layout.server.ts
@@ -1,7 +1,12 @@
-import { error } from "@sveltejs/kit";
+import { isRoleAdmin } from "$lib/utils";
+import { error, redirect } from "@sveltejs/kit";
 import type { LayoutServerLoad } from "./$types";
 
 export const load: LayoutServerLoad = async ({ locals, params }) => {
+  if (!locals.user || !isRoleAdmin(locals.user.role)) {
+    throw redirect(301, `/artists/${params.id}`);
+  }
+
   const res = await locals.apiClient.getArtistById(params.id);
   if (!res.success) {
     throw error(res.error.code, { message: res.error.message });
