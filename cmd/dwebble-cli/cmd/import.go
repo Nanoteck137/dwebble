67269bb37de3d235ee9f7451b1ce1474bdd759fe
Patrik M. Rosenström <patrik.millvik@gmail.com>
2025-02-15 15:00:15 +0100

    Change token default

--- cmd/dwebble-cli/cmd/import.go
+++ cmd/dwebble-cli/cmd/import.go
@@ -484,7 +484,7 @@ var importCmd = &cobra.Command{
 }
 
 func init() {
-	importCmd.Flags().StringP("token", "t", ".", "Api Token to use")
+	importCmd.Flags().StringP("token", "t", "", "Api Token to use")
 	importCmd.Flags().StringP("dir", "d", ".", "Directory to search")
 	importCmd.Flags().BoolP("open", "p", false, "Open in Browser")
 	importCmd.Flags().BoolP("extract", "e", false, "Force extract track number from filename")
