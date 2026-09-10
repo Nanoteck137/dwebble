c00b815fefa1f9a55d6770fcb3fa52bbdaa91710
Patrik M. Rosenström <patrik.millvik@gmail.com>
2026-09-10 15:55:49 +0200

    Working on a new frontend, with updated stuff and new UI library

---
+++ webnew/src/routes/+layout.svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+  import "../app.css";
+  import favicon from "$lib/assets/favicon.svg";
+  import { Tooltip } from "$lib/components/ui";
+  import { ModeWatcher } from "mode-watcher";
+
+  let { children } = $props();
+</script>
+
+<svelte:head>
+  <link rel="icon" href={favicon} />
+</svelte:head>
+
+<ModeWatcher />
+
+<Tooltip.Provider>
+  {@render children()}
+</Tooltip.Provider>
